8f4f520ddafa50486984573a8dc11ef1031d2327
gcrevell <gcrevell@mtu.edu>
2014-09-18 18:55:26 -0400

    Working on getting a week of events at a time

--- src/Calendar.java
+++ src/Calendar.java
@@ -31,10 +31,11 @@ public class Calendar implements Serializable {
 	
 	private static final long serialVersionUID = -147953699231233621L;
 
-	ArrayList<Event> events = new ArrayList<Event>();
+	TreeSet<Event> events = new TreeSet<Event>();
 	
 	public static final DateTime FIRST_SUNDAY = new DateTime("8-31-2014 12:00 am");
 	public static final DateTime LAST_SATURDAY = new DateTime("12-20-2014 11:59 pm");
+	
 	public boolean deleteEvent(Event e) {
 		return false;
 	}
@@ -52,7 +53,29 @@ public class Calendar implements Serializable {
 	}
 	
 	public Event[][] getWeek(DateTime startDay) {
+		if (startDay.compareTo(FIRST_SUNDAY) < 0 || startDay.compareTo(LAST_SATURDAY) > 0) {
+			return null;
+		}
+		
 		//Check if date is a sunday
+		DateTime check = FIRST_SUNDAY;
+		DateTime last = FIRST_SUNDAY; 
+		
+		while (check.compareTo(LAST_SATURDAY) < 0) {
+			if (check.compareTo(startDay) > 0) {
+				break;
+			}
+			
+			last = check;
+			check.incrementWeek();
+		}
+		
+		Event[][] ret = new Event[7][0];
+		check = new DateTime(last.toString());
+		
+		for (int i = 0; i < 7; i++) {
+			
+		}
 		
 		return null;
 	}
